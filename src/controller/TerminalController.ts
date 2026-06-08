a6b47e0b0409ed71727f931a1ce4bf681aa4a41f
angelosartoneto <angelosartoneto@outlook.com>
2026-06-08 02:33:29 -0300

    Revert "fix/structure folder src project"
    This reverts commit cccd404097e954b69ad3e2e979912ceef6ce1a0a.

--- src/controller/TerminalController.ts
+++
@@ -1,7 +0,0 @@
-// Camada de Interface do Usuário
-
-import { iniciarMenu } from "../services/MenuService";
-
-export async function executarFluxoPrincipal(): Promise<void> {
-  await iniciarMenu();
-}
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-// RF13 - Ponto de entrada. Instancia os serviços e inicia o fluxo principal.
-
-import { executarFluxoPrincipal } from "./controller/TerminalController";
-
-async function main(): Promise<void> {
-  await executarFluxoPrincipal();
-}
-
-main();
